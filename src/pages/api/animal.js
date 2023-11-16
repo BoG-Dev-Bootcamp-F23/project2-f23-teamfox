8ac4938ac05ba9ea93fe8127c411bc4f71905107
josephshenq <josephshenq@gmail.com>
2023-11-16 18:48:32 -0500

    endpoints update

--- src/pages/api/animal.js
+++ src/pages/api/animal.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
         } catch (e) {
             console.log(e.message.toString());
             if (e.message.toString() === "Error: Animal Not Found") {
-                return res.status(400).json({"status": "User Not Found."});
+                return res.status(400).json({"status": "Animal Not Found."});
             }
             else return res.status(500).json({"status": "Failed to update animal due to database issues."});
         }
