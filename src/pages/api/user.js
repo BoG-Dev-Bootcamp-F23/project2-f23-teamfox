2b9313f884a597b794f3756bfe98299f36c69866
Alex Li <ali602@gatech.edu>
2023-11-16 19:09:24 -0500

    updated files

---
+++ src/pages/api/user.js
@@ -0,0 +1,24 @@
+import updateAnimal from "../../../server/mongodb/actions/updateAnimal.js";
+
+export default async function handler(req, res) {
+    if (req.method === "POST") {
+        req.body 
+    } else if (req.method === "PATCH") {
+        try {
+            const { animalID, addValue } = req.body;
+            const response = await updateAnimal(animalID, addValue);
+            // console.log(response);
+            return res.status(200).json({"status": "success"});
+        } catch (e) {
+            console.log(e.message.toString());
+            if (e.message.toString() === "Error: Animal Not Found") {
+                return res.status(400).json({"status": "Animal Not Found."});
+            }
+            else return res.status(500).json({"status": "Failed to update animal due to database issues."});
+        }
+    } else if (req.method === "GET") {
+        req.query
+    } else if (req.method === "DELETE") {
+        req.query
+    }
+}
